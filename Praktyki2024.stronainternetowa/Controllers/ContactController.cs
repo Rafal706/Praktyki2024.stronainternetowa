270a786fcc20ad8f7bd0d3aa0949a26cbdd1df96
Rafal706 <rafalkukulski42@gmail.com>
2024-10-03 11:57:19 +0200

    .

---
+++ Praktyki2024.stronainternetowa/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Praktyki2024.stronainternetowa.Models;
+
+namespace Praktyki2024.stronainternetowa.Controllers
+{
+    public class ContactController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ContactFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction("ThankYou");
+            }
+            return View(model);
+        }
+
+        
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Praktyki2024.stronainternetowa/Models/OpinionFormModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Praktyki2024.stronainternetowa.Models
+{
+    public class OpinionFormModel
+    {
+        [Required(ErrorMessage = "Imię jest wymagane.")]
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Adres e-mail jest wymagany.")]
+        [EmailAddress(ErrorMessage = "Nieprawidłowy adres e-mail.")]
+        [Display(Name = "Adres E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Ocena jest wymagana.")]
+        [Range(1, 5, ErrorMessage = "Ocena musi być w zakresie od 1 do 5.")]
+        [Display(Name = "Ocena (1-5)")]
+        public int Rating { get; set; }
+
+        [Required(ErrorMessage = "Opinia jest wymagana.")]
+        [StringLength(500, ErrorMessage = "Opinia nie może mieć więcej niż 500 znaków.")]
+        [Display(Name = "Opinia")]
+        public string Opinion { get; set; }
+
+        [StringLength(1000, ErrorMessage = "Komentarz nie może mieć więcej niż 1000 znaków.")]
+        [Display(Name = "Komentarze/Wskazówki")]
+        public string AdditionalComments { get; set; }
+    }
+}
